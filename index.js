57d1fa7fded8e05d21d7b2714fbf1de96795d90a
Emmanuel Nduhiu <nduhiuemmanuel@gmail.com>
2025-09-25 20:04:28 +0300

    Completed the lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+// 1. Declare customerName in global scope
+var customerName = "bob";
+
+// 2. Function that uppercases the global customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// 3. Function that sets a global bestCustomer
+function setBestCustomer() {
+  bestCustomer = "not bob"; // no var/let/const → creates global
+}
+
+// 4. Function that overwrites bestCustomer
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+// 5. Declare a constant in global scope
+const leastFavoriteCustomer = "someone";
+
+// 6. Function that tries to change the const
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "new person"; // will throw an error
+}
